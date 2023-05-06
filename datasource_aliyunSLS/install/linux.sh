3016a0fe9d0cb115d493a48389e9e048d1cdb0f6
鬼画符 <mail@xnx3.com>
2023-05-06 21:48:00 +0800

    aliyunSLS v1.0.1 ，增加install.sh

---
+++ datasource_aliyunSLS/install/linux.sh
@@ -0,0 +1,6 @@
+# linux系统中，加入 aliyunOSS服务的jar
+
+# log-datasource-aliyunSLS-1.0.jar
+cd /mnt/tomcat8/webapps/ROOT/WEB-INF/lib/
+rm -rf /mnt/tomcat8/webapps/ROOT/WEB-INF/lib/log-datasource-aliyunSLS-*.jar
+wget https://gitee.com/mail_osc/FileUpload/raw/main/storage_aliyunOSS/install/lib/log-datasource-aliyunSLS-1.0.jar
